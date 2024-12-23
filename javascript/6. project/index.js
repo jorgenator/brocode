012f3d7737be8f7615747166cd0a612368e972c7
jorgenator <jromerog0802@gmail.com>
2024-12-23 02:49:24 +0100

    adding am and pm abbreviations

--- javascript/6. project/index.js
+++ javascript/6. project/index.js
@@ -1,7 +1,8 @@
 let clock = document.getElementById("myTime");
+
 function runClock() {
   let now = new Date();
-  clock.textContent = now.toLocaleTimeString();
+  clock.textContent = now.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
 }
 
 setInterval(runClock, 1000);

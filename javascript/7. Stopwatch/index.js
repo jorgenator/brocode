f47d0047425cb4345b048ff8d55249347e5a13c8
jorgenator <jromerog0802@gmail.com>
2024-12-27 23:55:31 +0100

    adding miliseconds to stopwatch

--- javascript/7. Stopwatch/index.js
+++ javascript/7. Stopwatch/index.js
@@ -3,30 +3,33 @@ const stopBtn = document.getElementById(`stopBtn`);
 const resetBtn = document.getElementById(`resetBtn`);
 const display = document.getElementById(`display`);
 
-let startTime = new Date();
-startTime.setHours(0,0,0,0);
-let timestamp;
+let timer = new Date();
+timer.setHours(0,0,0,0);
+let runTimer;
 
 startBtn.onclick = () => {
     console.log( `Start Button Clicked` );
 
-    timestamp = setInterval( () => {
-        startTime.setSeconds(startTime.getSeconds() + 1);
-        display.textContent = startTime.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false });
-    }, 1000 );
+    runTimer = setInterval( () => {
+        timer.setMilliseconds(timer.getMilliseconds() + 10);
+        let hours = String(timer.getHours()).padStart(2, '0');
+        let minutes = String(timer.getMinutes()).padStart(2, '0');
+        let seconds = String(timer.getSeconds()).padStart(2, '0');
+        let milliseconds = String(Math.floor(timer.getMilliseconds() / 10)).padStart(2, '0');
+        display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+    }, 10 );
     
 }
 
 stopBtn.onclick = () => {
-    running = false;
-    clearInterval(timestamp);
     console.log( `Stop Button Clicked` );
+    clearInterval(runTimer);
 }
 
 resetBtn.onclick = () => {
     console.log( `Reset Button Clicked` );
-    clearInterval(timestamp);
-    startTime.setHours(0,0,0,0);
-    display.textContent = startTime.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false });
+    clearInterval(runTimer);
+    timer.setHours(0,0,0,0);
+    display.textContent = timer.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false }) + `:00`;
 }
 
